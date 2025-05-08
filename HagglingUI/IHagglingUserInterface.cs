e4489c9f748e91c2197239c8fffd0e8ff3f045ec
MarcUs7i <96580944+MarcUs7i@users.noreply.github.com>
2025-05-08 21:03:43 +0200

    Added interface and model files

--- HagglingUI/IHagglingUserInterface.cs
+++ HagglingUI/IHagglingUserInterface.cs
@@ -1,6 +1,50 @@
-﻿namespace HagglingUI;
+﻿using Haggling;
+
+namespace HagglingUI;
 
 public interface IHagglingUserInterface
 {
-    //TODO
+    /// <summary>
+    /// Prints the customer asking for a specific type of product
+    /// </summary>
+    /// <param name="customerAsking"> customer asking for a product </param>
+    /// <param name="productType"> the product customer is looking for </param>
+    void PrintInitialQuestion(IHuman customerAsking, ProductType productType);
+    
+    /// <summary>
+    /// Prints the action the customer is taking and
+    /// a line of dialogue based on the customer and if needed the offer they are making
+    /// </summary>
+    /// <param name="dialogue"> dialogue to select </param>
+    /// <param name="personTalking"> person talking </param>
+    /// <param name="offer"> offer being made </param>
+    void PrintDialogue(Dialogue dialogue, IHuman personTalking, Offer? offer = null);
+
+    /// <summary>
+    /// Prints are necessary trade details
+    /// </summary>
+    /// <param name="customer"> customer in the trade </param>
+    /// <param name="vendor"> vendor in the trade </param>
+    /// <param name="product"> product being traded for </param>
+    void PrintTradeDetails(IHuman customer, IHuman vendor, Product product);
+
+    /// <summary>
+    /// Prints the final result of the trade
+    /// </summary>
+    /// <param name="customer"> customer in the trade </param>
+    /// <param name="vendor"> vendor in the trade </param>
+    /// <param name="finalOffer"> final offer in the trade </param>
+    void PrintTradeResult(IHuman customer, IHuman vendor, Offer finalOffer);
+
+    /// <summary>
+    /// Prints the introduction of a person e.g. "John, a local peasant"
+    /// </summary>
+    /// <param name="person"></param>
+    void PrintPersonIntroduction(IHuman person);
+
+    /// <summary>
+    /// Prints all useful info about a person
+    /// </summary>
+    /// <param name="person"></param>
+    void PrintPersonInfo(IHuman person);
 }
