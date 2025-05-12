5f3c8d145294ae3b1ff0723f79191608d45d3ca6
MarcUs7i <96580944+MarcUs7i@users.noreply.github.com>
2025-05-12 15:37:37 +0200

    Improved project setup

---
+++ HagglingUI/Screen/ConsoleCheck.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HagglingUI.Screen;
+
+public static class ConsoleCheck
+{
+    [DllImport("libc")]
+    private static extern int isatty(int fd);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool AllocConsole();
+
+    public static bool IsConsoleAttached()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return GetConsoleWindow() != IntPtr.Zero;
+        }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
+                 RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return isatty(1) != 0;
+        }
+
+        return false;
+    }
+
+    public static void EnsureConsole()
+    {
+        if (IsConsoleAttached())
+        {
+            return;
+        }
+        
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            AllocConsole();
+        }
+        else
+        {
+            throw new Exception($"Console is not attached! Can't get console on {RuntimeInformation.OSDescription}");
+        }
+    }
+}
